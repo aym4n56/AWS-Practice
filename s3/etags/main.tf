ebe9f618f50ca569264b310a3d57ba7204e1efb8
Ayman El Mahdi <ayman.elmahdi10@outlook.com>
2026-01-19 20:19:44 +0000

    finished adding etag example on s3 object using terraform.

--- s3/etags/main.tf
+++ s3/etags/main.tf
@@ -8,14 +8,16 @@ terraform {
 }
 
 provider "aws" {    
-  
+  region = "eu-west-2"
 }
 
-resource "aws_s3_bucket" "default" {
+resource "aws_s3_bucket" "default" {  
 }
 
 resource "aws_s3_object" "object" {
-  bucket = resource.aws_s3_bucket.default
+  bucket = aws_s3_bucket.default.id
   key    = "aymansFile.txt"
   source = "aymansFile.txt"
+
+  etag = filemd5("aymansFile.txt")
 }
